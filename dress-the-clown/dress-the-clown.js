2e6a34d249fee8e36c21956b3b47b4a4873a3160
Mike <mikesymonnz@gmail.com>
2022-05-17 11:35:02 +1200

    updownleftright working

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,26 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    if (e.keyCode == '38') {
+      console.log("I pushed up")
+    //UP ARROW   
+    }
+    if (e.keyCode == '40') {
+      console.log("I pushed down")
+      //DOWN ARROW   
+    } 
+    if (e.keyCode == '37') {
+      console.log("I pushed left")
+        //LEFT ARROW   
+    }   
+    if (e.keyCode == '39') {
+      console.log("I pushed right")
+      //RIGHT ARROW   
+      } 
+}
